9b8c153bd2f5a135c41bafe732f782568d8e3f44
Yaroslava Mala <ymala@kse.org.ua>
2023-05-12 10:33:06 +0300

    initial commit 2.0

---
+++ sixth/Program.cs
@@ -0,0 +1,18 @@
+﻿var x = new BigInteger("1313234242425");
+Console.WriteLine(x);
+
+public class BigInteger
+{
+    private int[] _numbers;
+
+    public BigInteger(string value)
+    {
+        // convert here string representation to inner int array IN REVERSED ORDER
+        _numbers = new int[value.Length];
+
+        for (var i = value.Length - 1; i >= 0; i--)
+        {
+            _numbers[value.Length - 1 - i] = int.Parse(value[i].ToString());
+        }
+    }
+}
