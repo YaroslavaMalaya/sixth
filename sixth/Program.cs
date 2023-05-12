3ddb81a16cefe4d81d9b44221447322324a19e32
smazepaa <smazepa@kse.ord.ua>
2023-05-12 19:25:14 +0300

    method ToString()

--- sixth/Program.cs
+++ sixth/Program.cs
@@ -15,4 +15,19 @@ public class BigInteger
             _numbers[value.Length - 1 - i] = int.Parse(value[i].ToString());
         }
     }
+    
+    public override string ToString()
+    {
+        // convert array back to string and return it
+        var str = new List<string>();
+        for (var i = _numbers.Length - 1; i >= 0; i--)
+        {
+            str.Add(Convert.ToString(_numbers[i]));
+        }
+        var numb = string.Join("", str);
+
+        return numb;
+    }
+    
+    
 }
