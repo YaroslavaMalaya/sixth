08ce6917af29cbaa51c5184e60fd89485ae2915b
Yaroslava Mala <ymala@kse.org.ua>
2023-05-15 10:10:44 +0300

    Karatsuba Algorithm (works, but not correct)

--- sixth/Program.cs
+++ sixth/Program.cs
@@ -1,5 +1,7 @@
-﻿var x = new BigInteger("1313234242425");
-var y = new BigInteger("17898456325");
+﻿// var x = new BigInteger("1313234242425");
+// var y = new BigInteger("17898456325");
+var x = new BigInteger("678");
+var y = new BigInteger("567");
 Console.WriteLine(x + y); // the same as Console.WriteLine(y.Add(x));
 if ((y - x).check)
 {
@@ -10,9 +12,7 @@ else
     Console.WriteLine(y - x);
 
 }
-/* var z = new BigInteger("97898456325");
-//var w = new BigInteger("3488989498");
-Console.WriteLine(z.Sub(w)); */
+Console.WriteLine(x * y);
 
 public class BigInteger
 {
@@ -147,6 +147,100 @@ public class BigInteger
         return resultBigInteger;
     }
     
+    
+    public BigInteger MultKaratsuba(BigInteger another)
+    {
+        // return new BigInteger, result of current * another
+        int[] firstNum = _numbers; 
+        int[] secondNum = another._numbers;
+        var power = Math.Min(firstNum.Length, secondNum.Length); 
+        int bm = (int)Math.Pow(10, power);
+
+        // це множення багатозначного числа на однозначне (а це є декілька разів додати)
+        if (firstNum.Length == 1)
+        {
+            var first = int.Parse(String.Join("", firstNum));
+            Array.Reverse(secondNum);
+            var number = new BigInteger(String.Join("", secondNum));
+            while (first > 0)
+            {
+                number += number;
+                first--;
+            } 
+            return number;
+        }
+        if (secondNum.Length == 1)
+        {
+            var first = int.Parse(String.Join("", secondNum));
+            Array.Reverse(firstNum);
+            BigInteger number1 = new BigInteger(String.Join("", firstNum));
+            while (first > 0)
+            {
+                number1 += number1;
+                first--;
+            } 
+            return number1;
+        }
+            
+        // це треба для того, щоб якщо це число і є найменшим по довжині, то ми могли взяти його 1 елемент спокійно (далі буде більш зрозуміло)
+        var carryX = 0; 
+        var carryY = 0;
+        if (firstNum.Length - power == 0)
+        {
+            carryX = 1;
+        }
+        if (secondNum.Length - power == 0)
+        {
+            carryY = 1;
+        }
+        
+        var x1 = new int[firstNum.Length - power + carryX]; // отут як і казала, завжди буде елемент 
+        var x2 = new int[power - carryX]; // відняти від степеня, бо інакше там будуть усі цифри навіть (з першим х1) ЯКЩО це знову ж таки найменше число по довжині
+        var y1 = new int[secondNum.Length - power + carryY];
+        var y2 = new int[power - carryY];
+        
+        Array.Copy(firstNum, power - carryX, x1, 0, firstNum.Length - power + carryX);
+        Array.Copy(firstNum, 0, x2, 0, power - carryX);
+        Array.Copy(secondNum, power - carryY, y1, 0, secondNum.Length - power + carryY);
+        Array.Copy(secondNum, 0, y2, 0, power - carryY);
+        
+        Array.Reverse(x1);
+        var x11 = new BigInteger(String.Join("", x1));
+        Array.Reverse(x2);
+        var x22 = new BigInteger(String.Join("", x2));
+        Array.Reverse(y1);
+        var y11 = new BigInteger(String.Join("", y1));
+        Array.Reverse(y2);
+        var y22 = new BigInteger(String.Join("", y2));
+        
+        var z0 = x22 * y22;
+        var z2 = x11 * y11;
+        var z1 = (x11 + x22)*(y11 + y22) - z2 - z0;
+        
+        // це по суті множення будь-якого числа на bm (на пряму не зможем помножити) 
+        var z22 = z2.ToString();
+        foreach (char el in Math.Pow(bm, 2).ToString())
+        {
+            if (el != '1')
+            {
+                z22 += "0";
+            }
+        }
+        var z11 = z1.ToString();
+        foreach (char el in bm.ToString())
+        {
+            if (el != '1')
+            {
+                z11 += "0";
+            }
+        }
+        
+        var result = new BigInteger(z22) + new BigInteger(z11) + z0;
+
+        return result;
+    }
+
     public static BigInteger operator +(BigInteger a, BigInteger b) => a.Add(b);
     public static BigInteger operator -(BigInteger a, BigInteger b) => a.Sub(b);
+    public static BigInteger operator *(BigInteger a, BigInteger b) => a.MultKaratsuba(b);
 }
