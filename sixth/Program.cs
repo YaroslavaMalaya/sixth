dc05b7e67c8dd79379e3f23e633d4a61b41d5517
Yaroslava Mala <ymala@kse.org.ua>
2023-05-15 20:36:49 +0300

    Karatsuba doesn't work :(

--- sixth/Program.cs
+++ sixth/Program.cs
@@ -1,24 +1,34 @@
-﻿// var x = new BigInteger("1313234242425");
-// var y = new BigInteger("17898456325");
-var x = new BigInteger("-25");
-var y = new BigInteger("4");
-Console.WriteLine(x + y); // the same as Console.WriteLine(y.Add(x));
-if ((y - x).check)
+﻿/*
+while (true)
 {
-    Console.WriteLine("-" + (y - x));
-}
-else
-{
-    Console.WriteLine(y - x);
+    Console.WriteLine("Enter your example (with space):");
+    var example = Console.ReadLine().Split(" ");
+    var firstN = example[0];
+    var oper = example[1];
+    var secondN = example[2];
+
+    if (firstN.Contains('-') || secondN.Contains('-'))
+    {
+        
+    }
 
 }
+*/
+// var x = new BigInteger("1313234242425");
+// var y = new BigInteger("17898456325");
+
+var x = new BigInteger("253333");
+var y = new BigInteger("4673");
+Console.WriteLine(x + y); // the same as Console.WriteLine(y.Add(x));
+Console.WriteLine(y - x);
+Console.WriteLine(x - y);
 Console.WriteLine(x * y);
 
 public class BigInteger
 {
     private int[] _numbers;
-    public bool check;
-    
+    private bool _negative = false;
+
     public BigInteger(string value)
     {
         // convert here string representation to inner int array IN REVERSED ORDER
@@ -39,7 +49,10 @@ public class BigInteger
             str.Add(Convert.ToString(_numbers[i]));
         }
         var numb = string.Join("", str);
-
+        if (_negative)
+        {
+            numb = "-" + numb;
+        }
         return numb;
     }
     
@@ -84,7 +97,10 @@ public class BigInteger
             result = newArray;
             Array.Reverse(result);
         }
-        
+        else
+        {
+            Array.Reverse(result);
+        }
         var stringResult = String.Join("", result);
         return new BigInteger(stringResult);
     }
@@ -100,7 +116,7 @@ public class BigInteger
         if (firstNum.Length < secondNum.Length)
         {
             Array.Resize(ref firstNum, secondNum.Length);
-            check = true;
+            _negative = true;
             (firstNum, secondNum) = (secondNum, firstNum);
         }
         else if (secondNum.Length < firstNum.Length)
@@ -113,7 +129,7 @@ public class BigInteger
             {
                 if (firstNum[i] < secondNum[i])
                 {
-                    check = true;
+                    _negative = true;
                     (firstNum, secondNum) = (secondNum, firstNum);
                     break;
                 }
@@ -140,9 +156,9 @@ public class BigInteger
 
         var sub = String.Join("", result);
         var resultBigInteger = new BigInteger(sub);
-        if (check)
+        if (_negative)
         {
-            resultBigInteger.check = true;
+            resultBigInteger._negative = true;
         }
         return resultBigInteger;
     }
@@ -153,7 +169,7 @@ public class BigInteger
         // return new BigInteger, result of current * another
         int[] firstNum = _numbers; 
         int[] secondNum = another._numbers;
-        var power = Math.Min(firstNum.Length, secondNum.Length); 
+        var power = Math.Min(firstNum.Length, secondNum.Length) - 1 ; 
         int bm = (int)Math.Pow(10, power);
 
         // це множення багатозначного числа на однозначне (а це є декілька разів додати)
@@ -183,27 +199,38 @@ public class BigInteger
         }
             
         // це треба для того, щоб якщо це число і є найменшим по довжині, то ми могли взяти його 1 елемент спокійно (далі буде більш зрозуміло)
-        var carryX = 0; 
-        var carryY = 0;
-        if (firstNum.Length - power == 0)
-        {
-            carryX = 1;
-        }
-        if (secondNum.Length - power == 0)
-        {
-            carryY = 1;
-        }
+        // var carryX = 0;
+        // var carryY = 0;
+        // if (firstNum.Length - power == 0)
+        // {
+        //     carryX = 1;
+        // }
+        // if (secondNum.Length - power == 0)
+        // {
+        //     carryY = 1;
+        // }
         
-        var x1 = new int[firstNum.Length - power + carryX]; // отут як і казала, завжди буде елемент 
-        var x2 = new int[power - carryX]; // відняти від степеня, бо інакше там будуть усі цифри навіть (з першим х1) ЯКЩО це знову ж таки найменше число по довжині
-        var y1 = new int[secondNum.Length - power + carryY];
-        var y2 = new int[power - carryY];
+        // var x1 = new int[firstNum.Length - power + carryX]; // отут як і казала, завжди буде елемент 
+        // var x2 = new int[power - carryX]; // відняти від степеня, бо інакше там будуть усі цифри навіть (з першим х1) ЯКЩО це знову ж таки найменше число по довжині
+        // var y1 = new int[secondNum.Length - power + carryY];
+        // var y2 = new int[power - carryY];
+        //
+        // Array.Copy(firstNum, power - carryX, x1, 0, firstNum.Length - power + carryX);
+        // Array.Copy(firstNum, 0, x2, 0, power - carryX);
+        // Array.Copy(secondNum, power - carryY, y1, 0, secondNum.Length - power + carryY);
+        // Array.Copy(secondNum, 0, y2, 0, power - carryY);
         
-        Array.Copy(firstNum, power - carryX, x1, 0, firstNum.Length - power + carryX);
-        Array.Copy(firstNum, 0, x2, 0, power - carryX);
-        Array.Copy(secondNum, power - carryY, y1, 0, secondNum.Length - power + carryY);
-        Array.Copy(secondNum, 0, y2, 0, power - carryY);
         
+        var x1 = new int[firstNum.Length - power ]; // отут як і казала, завжди буде елемент 
+        var x2 = new int[power]; // відняти від степеня, бо інакше там будуть усі цифри навіть (з першим х1) ЯКЩО це знову ж таки найменше число по довжині
+        var y1 = new int[secondNum.Length - power];
+        var y2 = new int[power];
+        
+        Array.Copy(firstNum, power, x1, 0, firstNum.Length - power);
+        Array.Copy(firstNum, 0, x2, 0, power );
+        Array.Copy(secondNum, power, y1, 0, secondNum.Length - power );
+        Array.Copy(secondNum, 0, y2, 0, power);
+
         Array.Reverse(x1);
         var x11 = new BigInteger(String.Join("", x1)); 
         Array.Reverse(x2);
